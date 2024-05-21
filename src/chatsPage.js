159d7088ce64cabf59972f899c689730f7ef3959
Tazrif Raim <tazrifraim@gmail.com>
2024-05-21 20:37:19 +0600

    Add files via upload

---
+++ src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"35ea8e1e-813f-44d5-a736-b498adb66099"}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
